fd9608db97ddd21a08dd89a342a4bdc9b7757a51
Michal Drobnik <drobnikmi@wp.pl>
2017-04-12 23:39:00 +0200

    init

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import 'angular';
+const css = require('../css/style.sass');
+
+
+angular.module('myApp',[
+    
+])
+
+
+angular.bootstrap(document, ['myApp']);
+
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
+var prod = process.env.Node_ENV === 'production';
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, "./"),
+        filename: 'app.bundle.js',
+    },
+    module: {
+        rules: [
+        {
+            test: /\.sass$/,
+            use: ExtractTextPlugin.extract({
+                fallbackLoader: 'style-loader',
+                loader:['css-loader','sass-loader'],
+                publicPath: './'
+            })
+        },
+        {
+            test: /\.js$/,
+            exclude: '/node_modules',
+            use: 'babel-loader'
+        },
+        {
+        test: /\.json$/,
+        loader: 'json'
+        }
+        ]
+    },
+    devServer: {
+        compress: true,
+        port: 8080,
+         historyApiFallback: {
+            index: 'index.html',
+            contentBase: './'
+
+         },
+        stats: "errors-only",
+        open: true
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "./"),
+        compress: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new ExtractTextPlugin('./style.css')
+    ]
+}
